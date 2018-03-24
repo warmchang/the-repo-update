f98f2de82df78117a7bd5e3f75175ce505070ef5
William Zhang <zhang.wanmin@zte.com.cn>
2018-03-24 12:49:26 +0800

    Add the processing of upstream repo.
    Signed-off-by: William Zhang <zhang.wanmin@zte.com.cn>

---
+++ repo-update.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+
+###########################################################
+# Script to update all git repositories in current folder #
+#                                                         #
+# v0.5                                                    #
+# By Killian Kemps                                        #
+# Contributors: Pascal Duez, William Zhang                #
+###########################################################
+
+list=`ls -d */`
+force_yes=false
+
+# Store user argument to force all repo update
+while :;
+do
+  case $1
+    in
+    -f|--force-yes) force_yes=true;;
+    -p|--prune-remote) prune_remote=true;;
+    -?*)
+      printf 'WARN: Unknown option (ignored): %s\n' "$1" >&2
+      ;;
+    *) break
+  esac
+  shift
+done
+
+function update {
+  printf "\n------> Updating '$Dir'"
+  cd $Dir
+  git stash > /tmp/repoUpdate
+  editedFiles=`cat /tmp/repoUpdate`
+  printf "\n"
+  echo $editedFiles
+  git checkout master
+  # git pull --rebase
+  git fetch --all
+  hasupstream=`git remote | grep "upstream"`
+  if [ "$hasupstream" = "upstream" ] ; then
+    git rebase upstream/master
+  else
+    git rebase origin/master
+  fi
+  if [ "$prune_remote" = true ] ; then
+    git remote update --prune
+  fi
+  git checkout -
+  if [[ $editedFiles != *"No local changes to save"* ]]
+  then
+    git stash pop
+  fi
+  cd -
+  printf "<------------------------------------>\n\n"
+}
+
+for Dir in $list
+do
+  while true
+  do
+    # Check if the folder is a git repo
+    if [[ -d "${Dir}/.git" ]]; then
+
+      # Update without prompt if yes forced
+      if [ "$force_yes" = true ] ; then
+        update
+        break;
+      # Otherwise prompt user asking for repo update
+      else
+        read -p "Update $Dir? [y/n/q] " answer
+
+        case $answer in
+          [yY]* ) update
+            break;;
+
+          [nN]* ) break;;
+
+          [qQ]* ) exit;;
+
+          * )  echo "Enter Y, N or Q, please.";;
+        esac
+      fi
+    else
+      break
+    fi
+  done
+done
